612039ca5ff384687470a31990eb703f21770557
WalterAurelio <aureliogareca.dev@gmail.com>
2025-10-04 21:26:52 -0300

    Se realizó la configuración de la conexión a la base de datos.

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import connectDatabase from './config/dbConnect';
+import mongoose from 'mongoose';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Conectar base de datos
+connectDatabase();
+
+// Middlewares
+app.use(express.json());
+
+//
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hola mundo' });
+});
+
+// Escuchar puerto
+mongoose.connection.once('open', () => {
+  console.log('Conectado exitosamente a MongoDB');
+  app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}...`);
+  });
+});
