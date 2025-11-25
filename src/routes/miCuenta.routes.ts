b5a6487a543af2d33f7498e2bb76ca20cb24670d
unknown <elicontti@gmail.com>
2025-11-24 22:30:08 -0300

    guardo un cambio en mi cuenta

--- src/routes/miCuenta.routes.ts
+++ src/routes/miCuenta.routes.ts
@@ -4,16 +4,11 @@ import { miCuentaController } from '../controllers/miCuenta.controller';
 const router = Router();
 
 router.get('/mi-cuenta', miCuentaController.getMiCuenta);
-<<<<<<< HEAD
-router.post('/mi-cuenta/cbu', miCuentaController.registrarCbu);
-router.put('/mi-cuenta/cbu', miCuentaController.setCbuPrincipal);
-router.put('/mi-cuenta/cbu-principal/:cbu', miCuentaController.editarCbu);
-//router.delete('/mi-cuenta/cbu-principal/:cbu', miCuentaController.eliminarCbu);
 
-
-=======
 router.post('/mi-cuenta/cbu', miCuentaController.registerCbu);
 router.put('/mi-cuenta/cbu', miCuentaController.setMainCbu);
->>>>>>> 0c81c77b4b12637c1dd7fc679fb5d0d06fb41492
+router.put('/mi-cuenta/cbu-principal/:cbu', miCuentaController.editCbu);
+//router.delete('/mi-cuenta/cbu-principal/:cbu', miCuentaController.eliminarCbu);
+
 
 export default router;
